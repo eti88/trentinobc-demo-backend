92f1c18b47c900225a0ce608191f229493a80fe9
Etienne Tomaselli <info@infoet.it>
2020-06-05 00:32:31 +0200

    fix login controller

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API\Auth;
 
-use App\Models\User;
+use App\User;
 
 use Hash;
 use Validator;
@@ -33,16 +33,8 @@ class LoginController extends Controller
 
             if (Hash::check($request->password, $user->password)) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                
-                $currentRole = $user->roles->pluck('name')[0];
-                if (setting('logging')) {
-                    activity()
-                        ->causedBy($user)
-                        ->withProperties(['type' => 'login'])
-                        ->log('Ha effettuato l\'accesso.');
-                }
 
-                $response = ['token' => $token, 'user' => $user, 'role' => $currentRole];
+                $response = ['token' => $token, 'user' => $user];
                 return response()->json(['code' => 200, 'message' => 'Login effettuato.', 'data' => $response, 'error' => null], 200);
             } else {
                 return response()->json(['code' => 422, 'message' => 'Password errata.', 'data' => null, 'error' => null], 422);
@@ -56,15 +48,11 @@ class LoginController extends Controller
     public function me ()
     {
         $user = Auth::user();
-        $roles = $user->getRoleNames();
-        $permissions = $user->getAllPermissions();
         return response()->json([
             'code' => 200,
             'message' => 'Profilo utente.',
             'data' => [
-                'user' => $user,
-                'roles' => $roles,
-                'permissions' => $permissions
+                'user' => $user
             ],
             'error' => null
         ], 200);
@@ -75,14 +63,6 @@ class LoginController extends Controller
         $user = Auth::user();
         $token = $user->token();
         $token->revoke();
-
-        if (setting('logging'))
-        {
-            activity()
-                ->causedBy($user)
-                ->withProperties(['type' => 'logout'])
-                ->log('Ha effettuato il logout.');
-        }
 
         return response()->json(['code' => 200, 'message' => 'Logout con successo.', 'data' => null, 'error' => null], 200);
     
