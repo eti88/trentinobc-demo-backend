2575f43b43ad3477ab9437ab44590a95c448978a
Etienne Tomaselli <info@infoet.it>
2020-06-05 02:26:20 +0200

    fix concatenation string

--- app/Http/Controllers/API/BusinessController.php
+++ app/Http/Controllers/API/BusinessController.php
@@ -20,7 +20,7 @@ class BusinessController extends Controller
         if($validator->fails()) {
             return response()->json([
                 'code' => 422,
-                'message' => 'Richiesta non valida.',
+                'message' => 'Not valid request.',
                 'data' => null,
                 'error' => $validator->errors()
             ], 422); 
@@ -37,13 +37,14 @@ class BusinessController extends Controller
             $filename = $file->getClientOriginalName();
             $NEW_FILE_NAME = md5($filename . microtime());
             $path = $file->storeAs('tmp', $NEW_FILE_NAME . '.' . $file->getClientOriginalExtension());
-            
+            \Log::info('tmp file path: ' . $path);
             // vars
             $s1 = date('YmdHisu');
-            $s2 = date('YmdHisu') + 2;
-            $s3 = storage_path('app/tmp/') + 'meta.json';
+            $s2 = date('YmdHisu') - 22;
+            $s3 = storage_path('app/tmp/') . 'meta.json';
             shell_exec('RH1=$(/root/go/bin/bitsongcli keys show faucet -a --keyring-backend=test)');
             $cmd = '/root/go/bin/bitsongcli tx content add '. $s1 . ' '. $s2 . ' '. $s3 . ' '. $path . ' -y -o json --from faucet --stream-price 1ubtsg --download-price 10ubtsg --right-holder "100:$RH1"';
+            \Log::info($cmd);
             $confirmation = shell_exec($cmd);
 
             Storage::delete($path);
